7769f8f94ce5a1ca7f6ad79a7ad10c108f75364c
Erick López <ericklopeza90@gmail.com>
2020-05-11 12:09:40 -0500

    actualizacion 110520

--- webempresa/blog/views.py
+++ webempresa/blog/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
-from .models import Entradas
+from django.shortcuts import render, get_object_or_404
+from .models import Entradas, Category
 
 # Create your views here.
 
 def blog(request):
     entradass = Entradas.objects.all()
     return render(request, "blog/blog.html", {'entradass':entradass})
+
+def category(request, category_id):
+    category = get_object_or_404(Category, id=category_id)
+    return render(request, "blog/category.html", {'category':category})
